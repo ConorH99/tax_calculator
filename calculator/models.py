25a40cecaaffac047ae3652f55a771b48b49395c
Conor Heeney <conor.heeney56@gmail.com>
2025-10-13 23:44:28 +0100

    Set up database

--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Asset(models.Model):
+    name = models.CharField(max_length=255)
+    ticker = models.CharField(max_length=5)
+
+class Action(models.Model):
+    action = models.CharField(max_length=8)
+
+class Transaction(models.Model):
+    date = models.DateField()
+    purchase_amount = models.DecimalField(max_digits=7, decimal_places=2)
+    share_price = models.DecimalField(max_digits=5, decimal_places=2)
+    num_shares = models.DecimalField(max_digits=6, decimal_places=5)
+    gain_loss = models.DecimalField(max_digits=10, decimal_places=4)
+    action_id = models.ForeignKey(Action, on_delete=models.PROTECT)
+    asset_id = models.ForeignKey(Asset, on_delete=models.CASCADE)
+
--- tax_calculator/settings.py
+++ tax_calculator/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "calculator"
 ]
 
 MIDDLEWARE = [
@@ -74,8 +80,12 @@ WSGI_APPLICATION = "tax_calculator.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.getenv("CALCULATOR_DB_NAME"),
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": 3306,
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
     }
 }
 
