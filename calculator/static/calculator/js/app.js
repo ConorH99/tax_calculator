81b427acbd18863987ff9e57eb11bd5165f9939e
Conor Heeney <conor.heeney56@gmail.com>
2025-10-21 20:59:47 +0100

    Styling changes and JS refactoring

--- calculator/static/calculator/js/app.js
+++ calculator/static/calculator/js/app.js
@@ -8,22 +8,25 @@ add_transaction_button.addEventListener("click", () => {
     let new_row = transaction_table.insertRow(-1)
     
     for (let cell_num=1; cell_num<=num_cells; cell_num++) {
-        let new_cell = new_row.insertCell(-1)
-        new_cell.className = "p-2 w-1/8 truncate"
-
-        add_input_to_cell(new_cell, cell_num)
+        add_cell_to_row(cell_num, new_row)
     }
+
+    first_input = new_row.querySelector("input")
+    first_input.focus()
 })
 
-add_input_to_cell = (cell, cell_num) => {
+add_cell_to_row = (cell_num, row) => {
 
     let cols_without_input = [3, 5, 7, 8]
 
+    let new_cell = row.insertCell(-1)
+    new_cell.className = "p-2 w-1/8 truncate"
+
     if (!cols_without_input.includes(cell_num)) {
         let input = document.createElement("input")
-        input.className = "w-full text-center"
-        cell.appendChild(input)
+        input.className = "border border-black rounded-sm text-md p-1 w-full text-left"
+        new_cell.appendChild(input)
     } else {
-        cell.textContent = "-"
+        new_cell.textContent = "-"
     }
 }
