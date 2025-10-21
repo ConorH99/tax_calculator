5237617f33247c5121a9ad9bf34318b71463db9e
Conor Heeney <conor.heeney56@gmail.com>
2025-10-21 21:53:03 +0100

    Moved classes into separate variables

--- calculator/static/calculator/js/app.js
+++ calculator/static/calculator/js/app.js
@@ -17,14 +17,17 @@ add_transaction_button.addEventListener("click", () => {
 
 add_cell_to_row = (cell_num, row) => {
 
+    let cell_classes = "p-2 w-1/8 truncate"
+    let input_classes = "border border-black rounded-sm text-md p-1 w-full text-left"
+
     let cols_without_input = [3, 5, 7, 8]
 
     let new_cell = row.insertCell(-1)
-    new_cell.className = "p-2 w-1/8 truncate"
+    new_cell.className = cell_classes
 
     if (!cols_without_input.includes(cell_num)) {
         let input = document.createElement("input")
-        input.className = "border border-black rounded-sm text-md p-1 w-full text-left"
+        input.className = input_classes
         new_cell.appendChild(input)
     } else {
         new_cell.textContent = "-"
